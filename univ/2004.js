9e96815c5c1f74803f2e7c6b35b7f507df8a3182
MistQ <42373544+MistQ@users.noreply.github.com>
2018-08-14 17:24:27 +0800

    Create 2004.js

---
+++ univ/2004.js
@@ -0,0 +1,17 @@
+var univ_list = {
+    school: "华东师大",
+    id: "2004",
+    link: [ {
+        name: "主页",
+        url: "http://www.ecnu.edu.cn/"
+    }, {
+        name: "公共数据库",
+        url: "http://portal.ecnu.edu.cn/"
+    }, {
+        name: "图书馆",
+        url: "http://www.lib.ecnu.edu.cn/"
+    }, {
+        name: "大夏学堂",
+        url: "https://elearning.ecnu.edu.cn/"
+    } ]
+};
